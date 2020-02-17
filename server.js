29be5ce1d342b8fb5be87a81490b605f2e8eb9ec
Nolan Picini <nolanpic@gmail.com>
2020-02-17 13:21:25 -0800

    Add get by ID and create

--- server.js
+++ server.js
@@ -8,15 +8,51 @@ server.use(express.json());
 
 server.get('/accounts', (req, res) => {
     db()
-    .from('accounts')
-    .select('*')
-    .then(accounts => {
-        res.status(200).json(accounts);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({ error: "Something went wrong getting accounts" });
-    });
+        .from('accounts')
+        .select('*')
+        .then(accounts => {
+            res.status(200).json(accounts);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "Something went wrong getting accounts" });
+        });
+});
+
+server.get('/accounts/:id', (req, res) => {
+    const { id } = req.params;
+
+    getById(id)
+        .then(account => {
+            res.status(200).json(account);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "Something went wrong getting this account" });
+        });
+});
+
+server.post('/accounts', (req, res) => {
+    const account = req.body;
+
+    db('accounts')
+        .insert(account)
+        .then(inserted => {
+            return getById(inserted[0])
+                .then(newAccount => res.status(201).json(newAccount));
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "Something went wrong creating this account" });
+        });
 });
 
 module.exports = server;
+
+function getById(id) {
+    return db()
+        .from('accounts')
+        .select('*')
+        .where('id', id)
+        .first();
+}
