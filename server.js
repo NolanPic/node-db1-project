2cf003d96d99f3866af17d6253f7fa3880ae33bc
Nolan Picini <nolanpic@gmail.com>
2020-02-17 13:07:36 -0800

    Add get accounts

--- server.js
+++ server.js
@@ -6,4 +6,17 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/accounts', (req, res) => {
+    db()
+    .from('accounts')
+    .select('*')
+    .then(accounts => {
+        res.status(200).json(accounts);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "Something went wrong getting accounts" });
+    });
+});
+
 module.exports = server;
